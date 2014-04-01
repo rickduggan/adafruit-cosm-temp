c0235c852824e7853a19c03f0aacb475e1c9dce2
Rick Duggan <rick.duggan@gmail.com>
2014-04-01 02:10:26 +0000

    changed to daemon mode

--- start-temp.sh
+++ start-temp.sh
@@ -2,4 +2,4 @@
 # use forever to keep restarting app
 
 # -a = append logs, -f = stream logs to stdout
-forever --spinSleepTime=60000 -a -f -l ./adafruit-cosm-temp.log -o ./adafruit-cosm-temp.sys.log -e ./adafruit-cosm-temp.err.log -c sudo /usr/bin/python ./adafruit-cosm-temp.py
+forever --spinSleepTime=60000 -a -f -l ./adafruit-cosm-temp.log -o ./adafruit-cosm-temp.sys.log -e ./adafruit-cosm-temp.err.log start -c sudo /usr/bin/python ./adafruit-cosm-temp.py
